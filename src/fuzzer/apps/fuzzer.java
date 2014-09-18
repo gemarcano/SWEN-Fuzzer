b49428a49ba8ec74868e38c97bc1540096c18420
Gabriel Marcano <gabemarcano@yahoo.com>
2014-09-18 13:35:44 -0400

    Skeleton implementation of the command line parser.

--- src/fuzzer/apps/fuzzer.java
+++ src/fuzzer/apps/fuzzer.java
@@ -4,6 +4,10 @@ package fuzzer.apps;
  * @authors Connor <csh6900>, Gabriel <gem5597>
  *
  */
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class fuzzer {
 
 	/**
@@ -11,14 +15,25 @@ public class fuzzer {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Map<String, String> commandMap = parseCommandLine(args);
 	}
 
 	/***
 	 * Command line map "String" -> "String"
 	 * Keys -> Values:
-	 * 	mode -> "discover" or "test", always
+	 * 	mode -> "discover" or "test", empty if there is none (this should be an error)
 	 * 	cauth -> "username::password" or empty if there is none
 	 * 	cwords -> "filename" or empty if there is none
 	 */
+	static Map<String, String> parseCommandLine(String[] aCommandLine) {
+		Map<String, String> result = new HashMap<>();
+		result.put("mode", "");
+		result.put("cauth", "");
+		result.put("cwords", "");
+		
+		if (aCommandLine != null) {
+		
+		}
+		return result;
+	}
 }
