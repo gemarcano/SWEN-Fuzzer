7f1c9ff8dd1ac2d6fe48c634ba3a21325af2ab84
Connor <connorhack10@gmail.com>
2014-09-18 12:57:12 -0400

    File setup

---
+++ src/fuzzer/apps/fuzzer.java
@@ -0,0 +1,17 @@
+package fuzzer.apps;
+/**
+ * 
+ * @authors Connor <csh6900>, 
+ *
+ */
+public class fuzzer {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
