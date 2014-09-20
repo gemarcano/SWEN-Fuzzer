0e549d15bc5810fc0b2ad493e9d49355366a3741
Joe Ksiazek <hyzer.jak@gmail.com>
2014-09-20 17:14:14 -0400

    Added HtmlUnit libraries and created InputDiscovery class

---
+++ src/fuzzer/apps/InputDiscovery.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package fuzzer.apps;
+
+import java.util.ArrayList;
+
+/**
+ * @author Joe <jak3122>
+ * 
+ */
+public class InputDiscovery {
+	/**
+	 * 
+	 * @param url
+	 * @return
+	 */
+	public ArrayList<String> getInputs(String url) {
+		ArrayList<String> input_list = new ArrayList<String>();
+		
+		
+		return input_list;
+	}
+}
