30db158f1da07b1587f1efe1cde48379767da030
Joe Ksiazek <hyzer.jak@gmail.com>
2014-09-20 18:18:04 -0400

    Implemented getInputs logic.

--- src/fuzzer/apps/InputDiscovery.java
+++ src/fuzzer/apps/InputDiscovery.java
@@ -4,21 +4,37 @@
 package fuzzer.apps;
 
 import java.util.ArrayList;
+import java.util.List;
+
+import com.gargoylesoftware.htmlunit.html.DomElement;
+import com.gargoylesoftware.htmlunit.html.HtmlElement;
+import com.gargoylesoftware.htmlunit.html.HtmlForm;
+import com.gargoylesoftware.htmlunit.html.HtmlInput;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
 
 /**
  * @author Joe <jak3122>
  * 
  */
 public class InputDiscovery {
+	
 	/**
-	 * 
-	 * @param url
-	 * @return
+	 * Returns all input elements on a given HtmlPage.
+	 * @param page The page to get inputs from.
+	 * @return All inputs from page.
 	 */
-	public ArrayList<String> getInputs(String url) {
-		ArrayList<String> input_list = new ArrayList<String>();
-		
-		
+	public List<HtmlElement> getInputs(HtmlPage page) {
+		List<HtmlElement> input_list = new ArrayList<HtmlElement>();
+		List<HtmlForm> formsList = page.getForms();
+		Iterable<DomElement> input_elements;
+		for (HtmlForm form : formsList) {
+			input_elements = form.getChildElements();
+			for (DomElement input : input_elements) {
+				if (input instanceof HtmlInput) {
+					input_list.add((HtmlInput)input);
+				}
+			}
+		}
 		return input_list;
 	}
 }
