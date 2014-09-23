d5e39dc8983c7c10b16f33b818ab5751fd0383cd
Gabriel Marcano <gabemarcano@yahoo.com>
2014-09-22 23:12:42 -0400

    Added class to help with automated login in.

---
+++ src/fuzzer/apps/PageLogin.java
@@ -0,0 +1,126 @@
+package fuzzer.apps;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.gargoylesoftware.htmlunit.ElementNotFoundException;
+import com.gargoylesoftware.htmlunit.html.DomElement;
+import com.gargoylesoftware.htmlunit.html.HtmlButton;
+import com.gargoylesoftware.htmlunit.html.HtmlForm;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+import com.gargoylesoftware.htmlunit.html.HtmlPasswordInput;
+import com.gargoylesoftware.htmlunit.html.HtmlSubmitInput;
+import com.gargoylesoftware.htmlunit.html.HtmlTextInput;
+
+public class PageLogin {
+	private Map<String,String> mKnownUsernames;
+	private Map<String,String> mKnownPasswords;
+	
+	public PageLogin()
+	{
+		mKnownUsernames = new HashMap<>();
+		mKnownPasswords = new HashMap<>();
+		
+		mKnownUsernames.put("dvwa", "admin");
+		mKnownPasswords.put("dvwa", "password");
+	}
+	
+	static private HtmlTextInput findUsernameField(HtmlPage aPage) 
+	{
+		HtmlTextInput result = null;
+		try {
+			result = aPage.getElementByName("username");
+		} catch (ElementNotFoundException e) {}
+		return result;
+	}
+	
+	static private HtmlPasswordInput findPasswordField(HtmlPage aPage) 
+	{
+		HtmlPasswordInput result = null;
+		try {
+			result = aPage.getElementByName("password");
+		} catch (ElementNotFoundException e) {}
+		return result;
+	}
+	
+	static private HtmlSubmitInput findLoginSubmitButton(HtmlPage aPage) 
+	{
+		HtmlSubmitInput result = null;
+		try {
+			result = (HtmlSubmitInput) aPage.getElementById("submit");
+		} catch (ElementNotFoundException e) {}
+		return result;
+	}
+	
+	/**	Returns true if logon succeeded. False otherwise.
+	 * 
+	 * @param aPage Page with login forms.
+	 * @return True upon sucessful login (assumes success if it cannot find login forms on the page loaded after the username password submit).
+	 */
+	public boolean logon(HtmlPage aPage)
+	{
+		boolean success = false;
+		
+		HtmlTextInput userInput;
+		HtmlPasswordInput passInput;
+		HtmlSubmitInput submit;
+		String username, password;
+		
+		//find username, password fields, and submit button
+		userInput = findUsernameField(aPage);
+		passInput = findPasswordField(aPage);
+		submit = findLoginSubmitButton(aPage);
+		
+		
+		if (userInput == null || passInput == null || submit == null)
+		{
+			return success; //No point in continuing if we can't find the login portion
+		}
+		
+		//Determine username and password combination to use
+		username = "admin";
+		password = "password";
+		
+		userInput.setText(username);
+		passInput.setText(password);
+		
+		//Attempt to log in.
+		HtmlPage newPage = null;
+		try {
+			newPage = submit.click();
+		} catch (IOException e) {
+			// FIXME what is a reasonable thing to do here?
+			e.printStackTrace();
+			return success;
+		}
+		
+		//Verify that we are logged in. This may be done via cookies or making sure that we have been moved to another page (one without login prompts).
+		if (findLoginSubmitButton(aPage) == null)
+		{
+			success = true;
+		}
+		
+		//Attempt to find login forms
+		//If found,
+		//	If a known site, use known information
+		//	Try to use a list of common username and password
+		//	try to bruteforce entry? (this should be a non-default option)
+		return success;
+	}
+	
+	private static boolean findForm(HtmlPage aPage, String aName)
+	{
+		boolean result;
+		List<HtmlForm> list = aPage.getForms();
+		try {
+			HtmlForm form = aPage.getFormByName(aName);
+			result = true;
+		} catch (ElementNotFoundException e) {
+			result = false;
+		}
+		return result;
+	}
+}
---
+++ src/fuzzer/apps/PageLoginTest.java
@@ -0,0 +1,42 @@
+package fuzzer.apps;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+
+import org.junit.Test;
+
+import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+
+public class PageLoginTest {
+
+	@Test
+	public void testPageLogin() {
+		PageLogin login = new PageLogin();
+	}
+
+	@Test
+	public void testLogon() {
+		PageLogin login = new PageLogin();
+		WebClient client = new WebClient();
+		HtmlPage page;
+		try {
+			page = client.getPage("http://localhost:8080/bodgeit/login.jsp");
+			assertTrue(login.logon(page));
+		} catch (FailingHttpStatusCodeException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
