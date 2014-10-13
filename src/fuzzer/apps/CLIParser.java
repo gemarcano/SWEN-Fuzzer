8309277e52ddc6183c96a95b7215e346dc4491a9
Gabriel Marcano <gem5597@rit.edu>
2014-10-13 13:40:39 -0400

    Improved option parsing implementation by leveraging third party Apache CLI library.

--- src/fuzzer/apps/CLIParser.java
+++ src/fuzzer/apps/CLIParser.java
@@ -1,8 +1,11 @@
 package fuzzer.apps;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.HashMap;
 import java.util.Map;
+
+import org.apache.commons.cli.*;
 
 public class CLIParser {
 
@@ -34,7 +37,12 @@ public class CLIParser {
 	 *         keys.
 	 */
 	public String get(String aKey) {
-		return mCLIParams.get(aKey);
+		String result = mCLIParams.get(aKey);
+		if (result == null)
+		{
+			result = "";
+		}
+		return result;
 	}
 
 	/**
@@ -61,36 +69,74 @@ public class CLIParser {
 		result.put("mode", "");
 		result.put("cauth", "");
 		result.put("cwords", "");
+		result.put("vectors", "");
+		result.put("sensitive", "");
+		result.put("random", "");
+		result.put("slow", "");
 		result.put("url", "");
 
-		if (aCommandLine != null && aCommandLine.length > 1) {
+		if (aCommandLine.length >= 2)
+		{
 			String command = aCommandLine[0];
-			if (command.equals("discover") /* || command.equals("test") */) {
+			if (command.equals("discover") || command.equals("test") ) {
 				result.put("mode", command);
 			}
 
 			result.put("url", aCommandLine[1]);
 
-			// For the rest of the parameters
-			for (int i = 2; i < aCommandLine.length; i++) {
-				String[] option = aCommandLine[i].split("=");
-				if (option.length <= 2) {
-					switch (option[0]) {
-					case "--custom-auth":
-						if (option.length == 2) {
-							result.put("cauth", option[1]);
-						}
-						break;
-					case "--common-words":
-						if (option.length == 2) {
-							result.put("cwords", option[1]);
-						}
-						break;
-					default:
-					} // What if get(0) is out of bounds?
+			if (aCommandLine.length > 2)
+			{
+				//FIXME parse options conditionally
+				Options opts = new Options();
+				//both
+				opts.addOption(OptionBuilder.withLongOpt("custom-auth").withValueSeparator().hasArg().create("cauth"));
+				
+				//discovery
+				opts.addOption(OptionBuilder.withLongOpt("common-words").withValueSeparator().hasArg().create("cwords"));
+				
+				//test
+				opts.addOption(OptionBuilder.withLongOpt("vectors").withValueSeparator().hasArg().create("vectors"));
+				opts.addOption(OptionBuilder.withLongOpt("sensitive").withValueSeparator().hasArg().create("sensitive"));
+				opts.addOption(OptionBuilder.withLongOpt("random").withValueSeparator().hasArg().create("random"));
+				opts.addOption(OptionBuilder.withLongOpt("slow").withValueSeparator().hasArg().create("slow"));
+				
+				CommandLineParser parser = new GnuParser();
+				CommandLine line = null;
+				
+
+				String[] restOfArgs = Arrays.copyOfRange(aCommandLine, 2, aCommandLine.length);
+				try {
+					line = parser.parse(opts, restOfArgs);
+				} catch (ParseException e) {
+					// FIXME leaking information!
+					e.printStackTrace();
 				}
+				
+				if (line != null) {
+					if (line.hasOption("cauth")) {
+						result.put("cauth", line.getOptionValue("cauth"));
+					}
+					if (line.hasOption("cwords")) {
+						result.put("cwords", line.getOptionValue("cwords"));
+					}
+					if (line.hasOption("vectors")) {
+						result.put("vectors", line.getOptionValue("vectors"));
+					}
+					if (line.hasOption("sensitive")) {
+						result.put("sensitive", line.getOptionValue("sensitive"));
+					}
+					if (line.hasOption("random")) {
+						result.put("random", line.getOptionValue("random"));
+					}
+					if (line.hasOption("slow")) {
+						result.put("slow", line.getOptionValue("slow"));
+					}
+				}
+				
+					
 			}
 		}
+		
 		return result;
 	}
 }
