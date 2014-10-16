66fc8a77270c148705fa9e64b30adcb77b6590ab
Connor <connorhack10@gmail.com>
2014-10-16 03:36:13 -0400

    Initial commit of XSS testing

---
+++ src/fuzzer/apps/VVector/XSSVectorTest.java
@@ -0,0 +1,38 @@
+package fuzzer.apps.VVector;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+
+import org.junit.Test;
+
+import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
+import com.gargoylesoftware.htmlunit.WebClient;
+import com.gargoylesoftware.htmlunit.html.HtmlPage;
+
+public class XSSVectorTest {
+
+	@Test
+	public void test() {
+		
+		WebClient client = new WebClient();
+		HtmlPage page;
+		try {
+			page = client.getPage("http://127.0.0.1/dvwa/login.php");
+			XSSVector vector = new XSSVector(page);
+			assertTrue(vector.test("test"));
+			
+		} catch (FailingHttpStatusCodeException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
