7e31fd87a0ad8140b8485dc2d265a58d6289d4b5
Gabriel Marcano <gem5597@rit.edu>
2014-10-16 04:43:59 -0400

    Fixed a bad merge with XSS_SQLVector. Fixed inverted logic in SantitizationVector.

--- src/fuzzer/apps/VVector/XSS_SQLVector.java
+++ src/fuzzer/apps/VVector/XSS_SQLVector.java
@@ -21,57 +21,19 @@ public class XSS_SQLVector extends VVector {
 	@Override
 	public boolean test() {
 		boolean result = false;
-		boolean success = false;
+
 		
 		List<HtmlPage> pages = InputManipulation.testInputsWithGivenString(mPage, vectorString);
 		
-		success = true;
 		for (HtmlPage page : pages){
 			int status = page.getWebResponse().getStatusCode();
 			if ( (status >= 200) && (status < 300)) {
-				success &= true;
+				result = true;
 			}
 		}
 		
-		if (success){
-			String overflowString = "";
-			int bound = 1<<4;
-			for (int i = 0; i < bound; i++)
-			{
-				overflowString += "abcdefghij";
-			}
-			
-			pages = InputManipulation.testInputsWithGivenString(mPage, overflowString);
-
-			for (HtmlPage page : pages){
-				result |= page.getWebResponse().getStatusCode() >= 400;
-			}
-			
-			overflowString = "";
-			bound = 1<<16;
-			for (int i = 0; i < bound; i++)
-			{
-				overflowString += "abcdefghij";
-			}
-			
-			pages = InputManipulation.testInputsWithGivenString(mPage, overflowString);
-			
-			for (HtmlPage page : pages){
-				result |= page.getWebResponse().getStatusCode() >= 400;
-			}
-		}
+		loadDescription(result ? "The test run found a problem." : "Test run did not find a problem.");
 		
-		//Get page back
-		//Now try to overflow.
-		if (success)
-		{
-			loadDescription("Normal run succeeded and " + (result ? "the test runs found a problem." : "test runs did not find a problem."));
-		}
-		else
-		{
-			loadDescription("Normal run did not succeed!!! Tests did not run, since baseline could not be established.");
-		}
-		
-		return success && result;
+		return result;
 	}
 }
